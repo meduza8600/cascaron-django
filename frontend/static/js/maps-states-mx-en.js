28ba44a07db8db40da728b566ebb459e972d2039
meduza8600 <meduza8600@gmail.com>
2023-07-20 12:27:13 -0400

    add content

---
+++ frontend/static/js/maps-states-mx-en.js
@@ -0,0 +1,348 @@
+const mxStates = {
+    "MEX2706": {
+        name: "Baja California"
+    },
+    "MEX2707": {
+        name: "Baja California Sur"
+    },
+    "MEX2708": {
+        name: "Coahuila"
+    },
+    "MEX2709": {
+        name: "Chihuahua"
+    },
+    "MEX2710": {
+        name: "Durango"
+    },
+    "MEX2711": {
+        name: "Sinaloa"
+    },
+    "MEX2712": {
+        name: "Sonora",
+        list: [
+            {
+                title: "Santa Ana - Altar Highway",
+                items: [
+                    "Local employees",
+                    "Local suppliers",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "San Luis Río Colorado - Estación Doctor Highway",
+                items: [
+                    "Local employees",
+                    "Local Providers",
+                    "Community Programs"
+                ]
+            }
+        ]
+    },
+    "MEX2713": {
+        name: "Zacatecas"
+    },
+    "MEX2714": {
+        name: "Nuevo León",
+        list: [
+            {
+                title: "La Gloria - San Fernando Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            }
+        ]
+    },
+    "MEX2715": {
+        name: "San Luis Potosí"
+    },
+    "MEX2716": {
+        name: "Tamaulipas",
+        list: [
+            {
+                title: "La Gloria - San Fernando Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "Altamira Port Terminal",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            }
+        ]
+    },
+    "MEX2717": {
+        name: "Aguascalientes",
+        list: [
+            {
+                title: "Aguascalientes Western Beltway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            }
+        ]
+    },
+    "MEX2718": {
+        name: "Colima",
+        list: [
+            {
+                title: "Armeria - Manzanillo Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            }
+        ]
+    },
+    "MEX2719": {
+        name: "Jalisco"
+    },
+    "MEX2720": {
+        name: "Michoacán",
+        list: [
+            {
+                title: "Michoacan Package",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "Zitácuaro - Lengua de Vaca Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "Morelia - Airport",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            }
+        ]
+    },
+    "MEX2721": {
+        name: "Nayarit"
+    },
+    "MEX2722": {
+        name: "Campeche"
+    },
+    "MEX2723": {
+        name: "Oaxaca"
+    },
+    "MEX2724": {
+        name: "Puebla",
+        list: [
+            {
+                title: "Suministros Especializados de Puebla Plant",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "Puebla Package",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "Atlixco - Jantelelco Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "Tlaxcala - Puebla Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "Puebla Elevated Viaduct",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            }
+        ]
+    },
+    "MEX2725": {
+        name: "Tabasco"
+    },
+    "MEX2726": {
+        name: "Tlaxcala",
+        list: [
+            {
+                title: "Tlaxcala - Puebla Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "Tlaxcala - San Martin - Texmelucan Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            }
+        ]
+    },
+    "MEX2727": {
+        name: "Mexico City",
+        list: [
+            {
+                title: "Atlipac Plant",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "Mexico - Toluca Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            }
+        ]
+    },
+    "MEX2728": {
+        name: "Guanajuato"
+    },
+    "MEX2729": {
+        name: "Guerrero"
+    },
+    "MEX2730": {
+        name: "Hidalgo",
+        list: [
+            {
+                title: "Real del Monte - Huasca Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            }
+        ]
+    },
+    "MEX2731": {
+        name: "State of Mexico",
+        list: [
+            {
+                title: "Tenango - Ixtapan de la Sal Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "Mexico - Toluca Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "Peñon - Texcoco Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "Piramides - Ecatepec - Peñon Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "La Raza - Indios Verdes - Santa Clara Viaduct",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            }
+        ]
+    },
+    "MEX2732": {
+        name: "Morelos",
+        list: [
+            {
+                title: "Siglo XXI Highway",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            }
+        ]
+    },
+    "MEX2733": {
+        name: "Querétaro"
+    },
+    "MEX2734": {
+        name: "Veracruz",
+        list: [
+            {
+                title: "El Prieto Bridge",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            },
+            {
+                title: "José López Portillo Bridge",
+                items: [
+                    "Local employees",
+                    "Local supplier",
+                    "Community programs"
+                ]
+            }
+        ]
+    },
+    "MEX2735": {
+        name: "Chiapas"
+    },
+    "MEX2736": {
+        name: "Quintana Roo"
+    },
+    "MEX2737": {
+        name: "Yucatán"
+    }
+}
---
+++ frontend/static/js/maps-states-mx.js
@@ -0,0 +1,332 @@
+const mxStates = {
+    "MEX2706": {
+        name: "Baja California"
+    },
+    "MEX2707": {
+        name: "Baja California Sur"
+    },
+    "MEX2708": {
+        name: "Coahuila"
+    },
+    "MEX2709": {
+        name: "Chihuahua"
+    },
+    "MEX2710": {
+        name: "Durango"
+    },
+    "MEX2711": {
+        name: "Sinaloa"
+    },
+    "MEX2712": {
+        name: "Sonora",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2713": {
+        name: "Zacatecas"
+    },
+    "MEX2714": {
+        name: "Nuevo León",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2715": {
+        name: "San Luis Potosí"
+    },
+    "MEX2716": {
+        name: "Tamaulipas",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2717": {
+        name: "Aguascalientes",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2718": {
+        name: "Colima",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2719": {
+        name: "Jalisco"
+    },
+    "MEX2720": {
+        name: "Michoacán",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2721": {
+        name: "Nayarit"
+    },
+    "MEX2722": {
+        name: "Campeche"
+    },
+    "MEX2723": {
+        name: "Oaxaca"
+    },
+    "MEX2724": {
+        name: "Puebla",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2725": {
+        name: "Tabasco"
+    },
+    "MEX2726": {
+        name: "Tlaxcala",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2727": {
+        name: "Ciudad de México",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2728": {
+        name: "Guanajuato"
+    },
+    "MEX2729": {
+        name: "Guerrero"
+    },
+    "MEX2730": {
+        name: "Hidalgo",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2731": {
+        name: "Estado de México",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2732": {
+        name: "Morelos",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2733": {
+        name: "Querétaro"
+    },
+    "MEX2734": {
+        name: "Veracruz",
+        list: [
+            {
+                title: "title 1",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            },
+            {
+                title: "title 2",
+                items: [
+                    "item 1",
+                    "item 2",
+                    "item 3"
+                ]
+            }
+        ]
+    },
+    "MEX2735": {
+        name: "Chiapas"
+    },
+    "MEX2736": {
+        name: "Quintana Roo"
+    },
+    "MEX2737": {
+        name: "Yucatán"
+    }
+}
